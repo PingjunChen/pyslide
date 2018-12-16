ac900ab5883e6df8ee94b5d6b53ac1a5f2104157
PingjunChen <chenpingjun@gmx.com>
2018-12-16 15:53:56 -0500

    using pytest as testing framework

---
+++ test/io/test_io.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+def test_create_pyramidal_img():
+    print("Hello IO")
+
+    assert 1 == 1, "IO Testing is passed"
---
+++ test/patch/test_patch.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+def test_wsi_patch_splitting():
+    print("Hello Patch")
+
+    assert 1 == 1, "Patch Testing is passed"

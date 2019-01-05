c8a174806721a46035df8f242951d1c1c0ed2624
PingjunChen <chenpingjun@gmx.com>
2019-01-05 15:23:59 -0500

    1. add load_wsi_head 2. add test functions for pyramid module.

--- pyslide/pyramid/_pyramid.py
+++ pyslide/pyramid/_pyramid.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import openslide
 
 
 __all__ = ["create_pyramidal_img",
+            "load_wsi_head",
             ]
 
 def create_pyramidal_img(img_path, save_dir):
-    """Convert normal image to pyramidal image.
+    """ Convert normal image to pyramidal image.
     """
 
     convert_cmd = "convert " + img_path
@@ -17,5 +19,9 @@ def create_pyramidal_img(img_path, save_dir):
     os.system(convert_cmd + convert_option + convert_dst)
 
 
-def read_pyramidal_img(wsi_img_path):
-    pass
+def load_wsi_head(wsi_img_path):
+    """ Load the header meta data of whole slide pyramidal image
+    """
+    wsi_header = openslide.OpenSlide(wsi_img_path)
+
+    return wsi_header
--- test/pyramid/test_pyramid.py
+++ test/pyramid/test_pyramid.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import numpy as np
 from os.path import dirname as opd
 from os.path import abspath as opa
 from os.path import join as opj
@@ -12,6 +13,16 @@ sys.path.insert(0, PRJ_PATH)
 from pyslide import pyramid
 
 def test_create_pyramidal_img():
-    img_path = "../data/Images/CropBreastSlide.tif"
-    save_dir = "../data/Slides"
+    img_path = os.path.join(PRJ_PATH, "test/data/Images/CropBreastSlide.tif")
+    save_dir = os.path.join(PRJ_PATH, "test/data/Slides")
     pyramid.create_pyramidal_img(img_path, save_dir)
+    assert os.path.exists(os.path.join(save_dir, "CropBreastSlide.tiff"))
+
+def test_load_wsi_head():
+    wsi_img_path = os.path.join(PRJ_PATH, "test/data/Slides/CropBreastSlide.tiff")
+    wsi_header = pyramid.load_wsi_head(wsi_img_path)
+
+    print("WSI level dimension info:")
+    level_num = wsi_header.level_count
+    for ind in np.arange(level_num):
+        print("level {:2d} size: {}".format(ind, wsi_header.level_dimensions[ind]))

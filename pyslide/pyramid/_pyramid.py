a9cdaf3e8223930472a484de4a052b317c54fc8d
PingjunChen <chenpingjun@gmx.com>
2019-01-06 21:56:33 -0500

    add the update of _pyramid.py

--- pyslide/pyramid/_pyramid.py
+++ pyslide/pyramid/_pyramid.py
@@ -10,18 +10,45 @@ __all__ = ["create_pyramidal_img",
 
 def create_pyramidal_img(img_path, save_dir):
     """ Convert normal image to pyramidal image.
+
+    Parameters
+    ----------
+    img_path: str
+        Whole slide image path (absolute path is needed)
+    save_dir: str
+        Location of the saved the generated pyramidal image with extension tiff,
+        (absolute path is needed)
+
+    Returns
+    -------
+    status: int
+        The status of the pyramidal image generation (0 stands for success)
+
+    Notes
+    -------
+    ImageMagick need to be preinstalled to use this function.
+    >>> sudo apt-get install imagemagick
+
+    Examples
+    --------
+    >>> img_path = os.path.join(PRJ_PATH, "test/data/Images/CropBreastSlide.tif")
+    >>> save_dir = os.path.join(PRJ_PATH, "test/data/Slides")
+    >>> status = pyramid.create_pyramidal_img(img_path, save_dir)
+    >>> assert status == 0
     """
 
     convert_cmd = "convert " + img_path
     convert_option = " -compress jpeg -quality 90 -define tiff:tile-geometry=256x256 ptif:"
     img_name = os.path.basename(img_path)
     convert_dst = os.path.join(save_dir, os.path.splitext(img_name)[0] + ".tiff")
-    os.system(convert_cmd + convert_option + convert_dst)
+    status = os.system(convert_cmd + convert_option + convert_dst)
 
+    return status
 
 def load_wsi_head(wsi_img_path):
-    """ Load the header meta data of whole slide pyramidal image
+    """ Load the header meta data of whole slide pyramidal image.
     """
+
     wsi_header = openslide.OpenSlide(wsi_img_path)
 
     return wsi_header

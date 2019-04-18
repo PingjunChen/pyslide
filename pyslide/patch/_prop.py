a435f9aec4f83d10858f20c13818b7021529454d
PingjunChen <chenpingjun@gmx.com>
2019-04-18 12:33:17 -0400

    add patch background ratio

--- pyslide/patch/_prop.py
+++ pyslide/patch/_prop.py
@@ -2,7 +2,7 @@
 
 import os, sys
 import numpy as np
-
+from skimage import color
 
 __all__ = ['mean_patch_val', ]
 
@@ -45,3 +45,28 @@ def std_patch_val(img):
     std_val = np.std(img)
 
     return std_val
+
+
+def patch_bg_ratio(img, bg_thresh=0.72):
+    """ Calculate the ratio of background in the image
+
+    Parameters
+    -------
+    img: np.array
+        patch image
+    bg_thresh: float
+        background threshold value
+
+    Returns
+    -------
+    bg_ratio: float
+        the ratio of background in a patch
+
+    """
+
+    g_img = color.rgb2gray(img)
+    bg_num = (g_img > bg_thresh).sum()
+    pixel_num = g_img.shape[0] * g_img.shape[1]
+    bg_ratio = bg_num * 1.0 / pixel_num
+
+    return bg_ratio

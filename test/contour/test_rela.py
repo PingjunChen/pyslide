7e06ea0229d7e1980ba578355405372a5421fbe8
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-04-02 17:16:40 +0200

    Increased readability for assertions

--- test/contour/test_rela.py
+++ test/contour/test_rela.py
@@ -11,15 +11,13 @@ sys.path.insert(0, PRJ_PATH)
 
 from pyslide import contour
 
-
 def test_cnt_inside_wsi():
     wsi_h, wsi_w = 4, 5
     cnt1 = np.array([[1, 0, 0, 1, 3.9, 3],
                      [0, 0, 1, 3, 4.9, 1]])
     in_flag = contour.cnt_inside_wsi(cnt1, wsi_h, wsi_w)
 
-    if in_flag != True:
-        raise AssertionError
+    assert in_flag == True, "The contour should be inside the WSI"
 
 
 def test_intersect_cnt_wsi():
@@ -28,8 +26,9 @@ def test_intersect_cnt_wsi():
                      [0, 0, 1, 3, 5.1, 1]])
 
     inter_cnt = contour.intersect_cnt_wsi(cnt1, wsi_h, wsi_w)
-    if np.max(inter_cnt[0, :]) >= wsi_h or np.max(inter_cnt[1, :]) >= wsi_w:
-        raise AssertionError
+    assert np.max(inter_cnt[0, :]) < wsi_h, "The contour should not exceed the height of the WSI"
+    assert np.max(inter_cnt[1, :]) < wsi_w, "The contour should not exceed the width of the WSI"
+
 
 def test_cnt_inside_ratio():
     cnt1 = np.array([[1, 1, 3, 3],
@@ -38,5 +37,5 @@ def test_cnt_inside_ratio():
                      [2, 9, 9, 2]])
 
     inside_ratio = contour.cnt_inside_ratio(cnt1, cnt2)
-    if inside_ratio < 0.0 or inside_ratio > 1.0:
-        raise AssertionError
+    assert 0.0 <= inside_ratio <= 1.0, "The inside ratio should be between 0 and 1"
+
